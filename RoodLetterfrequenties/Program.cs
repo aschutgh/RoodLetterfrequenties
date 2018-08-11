33c6d64f9a606070e299ad4614d12f74bd0eff26
Arthur Schut <aschutgh@gmail.com>
2018-08-11 19:20:34 +0200

    added reading textfile from argument

--- RoodLetterfrequenties/Program.cs
+++ RoodLetterfrequenties/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 
 // Rood Letterfrequenties
+// usage: dotnet RoodLetterfrequenties.dll nameoftextfile
 
 namespace RoodLetterfrequenties
 {
@@ -26,21 +27,19 @@ namespace RoodLetterfrequenties
             return CharFreq;
         }
 
-        // FIXME: tekstbestand inlezen
-        // FIXME: argument uitlezen
-
         static void Main(string[] args)
         {
+            Console.WriteLine("Letter frequenties:");
+            //Console.WriteLine(args[0]);
             Dictionary<char, int> CharFreq = new Dictionary<char, int>();
-            CharFreq = CountChar("Dit is een test.æ");
+            string text = System.IO.File.ReadAllText(args[0]);
+
+            CharFreq = CountChar(text);
 
             foreach(char c in CharFreq.Keys)
             {
-                Console.WriteLine("{0}    {1}", c, CharFreq[c]);
+                Console.WriteLine("{0} komt {1} keer voor.", c, CharFreq[c]);
             }
-
-            
-
         }
     }
 }
