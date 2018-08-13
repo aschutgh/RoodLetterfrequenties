dd59ed6f00ccf84fb81a543196f3e8f2249ac1e5
Arthur Schut <aschutgh@gmail.com>
2018-08-13 11:36:40 +0200

    Ignore certain characters and chech if there is an argument

--- RoodLetterfrequenties/Program.cs
+++ RoodLetterfrequenties/Program.cs
@@ -8,19 +8,22 @@ namespace RoodLetterfrequenties
 {
     class Program
     {
-        static Dictionary<char, int> CountChar(string textctc)
+        static Dictionary<char, int> CountChar(string textctc, string avoidchars)
         {
             Dictionary<char, int> CharFreq = new Dictionary<char, int>();
 
             foreach(char c in textctc)
             {
-                if (!CharFreq.ContainsKey(c))
+                if (!avoidchars.Contains(c.ToString()))  
                 {
-                    CharFreq.Add(c, 1);
-                }
-                else if (CharFreq.ContainsKey(c))
-                {
-                    CharFreq[c] += 1;
+                    if (!CharFreq.ContainsKey(c))
+                    {
+                        CharFreq.Add(c, 1);
+                    }
+                    else if (CharFreq.ContainsKey(c))
+                    {
+                        CharFreq[c] += 1;
+                    }
                 }
             }
 
@@ -29,12 +32,17 @@ namespace RoodLetterfrequenties
 
         static void Main(string[] args)
         {
+            if (args.Length == 0)
+            {
+                Console.WriteLine("Geef de naam van een tekstbestand als argument op.");
+                Environment.Exit(0);
+            }
             Console.WriteLine("Letter frequenties:");
-            //Console.WriteLine(args[0]);
             Dictionary<char, int> CharFreq = new Dictionary<char, int>();
+            string snaos = " \t\n\r.:;-+()*&^%$#@!<>/?|\\,`~"; // spaces, newlines and other stuff...
             string text = System.IO.File.ReadAllText(args[0]);
 
-            CharFreq = CountChar(text);
+            CharFreq = CountChar(text, snaos);
 
             foreach(char c in CharFreq.Keys)
             {
